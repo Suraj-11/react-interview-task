f5891276fe2359b2e0268885e43217e1346ce809
Suraj-11 <61373191+Suraj-11@users.noreply.github.com>
2025-02-21 02:49:01 +0530

    Update apiConnect.js

--- react-interview-ui-main-javascript/src/lib/apiConnect.js
+++ react-interview-ui-main-javascript/src/lib/apiConnect.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://backend:9000'
+const BASE_URL = 'http://localhost:9000'
 
 export const fetchAllWidgets = () => axios.get(`${BASE_URL}/v1/widgets`).then((response) => response.data);
 
